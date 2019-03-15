4a3c3dd29e149224a37def28a4d9267a5a5199a6
meinfr13nd <sarahsamora@mail.com>
2019-03-15 12:39:35 -0700

    added comments to the script

--- utctf19/assets/regularZips.py
+++ utctf19/assets/regularZips.py
@@ -10,15 +10,15 @@ import os
 
 curidx = 399
 while True:
-   curfile = "dir" + str(curidx) + "/archive.zip"
+   curfile = "dir" + str(curidx) + "/archive.zip" #current archive file
    f = ZipFile(curfile)
    curhint = "dir" + str(curidx) + "/hint.txt"
    with open(curhint) as hintfile:
-       pattern = hintfile.read().rstrip('\n')
+       pattern = hintfile.read().rstrip('\n') #load regex hint
    curdir = "dir" + str(curidx + 1)
-   os.makedirs("dir" + str(curidx + 1), exist_ok=True)
+   os.makedirs("dir" + str(curidx + 1), exist_ok=True) #make the next directory to extract to
    found = False
-   for item in sre.AllStrings(pattern):
+   for item in sre.AllStrings(pattern): #try all regex stings
        try:
            f.extractall(path=curdir, pwd=item.encode("utf-8"))
            print(item)
